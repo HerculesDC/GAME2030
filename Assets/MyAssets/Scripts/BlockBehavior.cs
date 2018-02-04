c7f68248cecf14d31f71c9948078c513c17b719d
HerculesDC <hercules.amphibolite@gmail.com>
2018-02-03 19:00:13 -0500

    Second commit on Sat

--- Assets/MyAssets/Scripts/BlockBehavior.cs
+++ Assets/MyAssets/Scripts/BlockBehavior.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class BlockBehavior : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool activated = false;
+
+    private GameObject blueBlock;
+    private GameObject yellowBlock;
+
+    void Awake() {
+        blueBlock = GameObject.Find("Inactive");
+        yellowBlock = GameObject.Find("Active");
+        blueBlock.SetActive(true);
+        yellowBlock.SetActive(false);
+    }
+    
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Player") && !activated) {
+            activated = true;
+            blueBlock.SetActive(false);
+            yellowBlock.SetActive(true);
+        }
+    }
 }
